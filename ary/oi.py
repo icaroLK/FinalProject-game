9fd887cda4a5e44a7cc470e369bd3b2190cd2570
Ary Farah <aryfarahs@gmail.com>
2023-05-16 15:25:02 -0300

    .

--- ary/oi.py
+++ ary/oi.py
@@ -2,37 +2,49 @@ import pygame
 
 pygame.init()
 
-janela = pygame.display.set_mode((1080, 720))
+l = 1080
+h = 640
+janela = pygame.display.set_mode((l, h))
 clock = pygame.time.Clock()
 pygame.display.set_caption("Joguinho")
 running = True
-dt = 0
-
 
 background = pygame.image.load("ary/imagens/espaco.png")
+back_pos = 0
+back_vel = 400
 
-pos = pygame.Vector2(janela.get_width() / 2, janela.get_height() / 2)
+player = pygame.image.load("ary/imagens/nave.png")
+pos = pygame.Vector2(l / 2, h / 2)
 
 while running:
+    dt = clock.tick(60) / 1000 
+
     for evento in pygame.event.get():
         if evento.type == pygame.QUIT:
             running = False
-    janela.blit(background, (0,0))    
-
-    pygame.draw.circle(janela, "red", pos, 40)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
-        pos.y -= 300 * dt
+        pos.y -= 250 * dt
     if keys[pygame.K_s]:
-        pos.y += 300 * dt
+        pos.y += 250 * dt
     if keys[pygame.K_a]:
-        pos.x -= 300 * dt
+        pos.x -= 250 * dt
     if keys[pygame.K_d]:
-        pos.x += 300 * dt
+        pos.x += 250 * dt
+
+    back_pos -= back_vel * dt
+    if back_pos < -background.get_width():
+        back_pos = 0
+
+    superficie = pygame.Surface((l + background.get_width(), h))
+    superficie.blit(background, (back_pos, 0)) 
+    superficie.blit(background, (back_pos + background.get_width(), 0))
+
+    janela.fill((0, 0, 0))
+    janela.blit(superficie, (0, 0))
+    janela.blit(player, pos - pygame.Vector2(l / 2, h / 2))
 
     pygame.display.flip()
-
-    dt = clock.tick(60) / 1000
 
 pygame.quit()
