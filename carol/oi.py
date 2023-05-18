ab49bc9dfeca41b7871b4a689347550e27ccf3d3
Caroline Assis <caroolassissss@gmail.com>
2023-05-18 09:15:45 -0300

    save

--- carol/oi.py
+++ carol/oi.py
@@ -2,20 +2,24 @@ import pygame
 from pygame.locals import *
 from sys import exit
 
-roxo = (255,131,250)
-rosa = (255,62,150)
-preto = (0,0,0)
-branco = (255,255,255)
-
-largura = 640
-altura = 480
+largura = 1280
+altura = 720
 
 pygame.display.set_mode((largura,altura))
-pygame.display.set_caption('ICARYANOLINE')
+pygame.display.set_caption('Ricks')
  
+
+pygame.font.init()
+font = pygame.font.Font(None, 36)
+text = font.render("Bem-vindo ao jogo Ricks", True, (255, 255, 255))
+background_image = pygame.image.load('carol/bg.jpg')
+screen = pygame.display.set_mode((largura, altura))
+
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             exit()
+
+    screen.blit(background_image, [0, 0])
     pygame.display.update()
